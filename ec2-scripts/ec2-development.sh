bb7d9cec2c01edb601fc418c2667b4058b156d3c
EC2 Default User <ec2-user@ip-10-0-1-58.us-west-2.compute.internal>
2020-06-22 17:05:28 +0000

    Added more of finished M4 project

--- ec2-scripts/ec2-development.sh
+++ ec2-scripts/ec2-development.sh
@@ -10,7 +10,7 @@ yum install -y git
 
 # Configure/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone https://github.com/scanlon771/python-image-gallery
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
