441b2a7bff52bee063fe0bb658ca79c58cda992d
EC2 Default User <ec2-user@ip-10-0-1-58.us-west-2.compute.internal>
2020-06-22 02:16:03 +0000

    added python stuff

---
+++ gallery/tools/db.py
@@ -0,0 +1,44 @@
+import psycopg2
+import json
+from secrets import get_secret_image_gallery
+
+connection = None
+
+def get_secret():
+    jsonString = get_secret_image_gallery()
+    return json.loads(jsonString)
+
+def get_password(secret):
+    return secret['password']
+
+def get_host(secret):
+    return secret['host']
+
+def get_username(secret):
+    return secret['username']
+
+def get_dbname(secret):
+    return secret['database_name']
+
+def connect():
+    global connection
+    secret = get_secret()
+    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
+
+def execute(query,args=None):
+    global connection
+    cursor = connection.cursor()
+    if not args:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, args)
+    return cursor
+
+def main():
+    connect()
+    res = execute('select * from users')
+    for row in res:
+        print(row)
+
+if __name__ == '__main__':
+    main()
